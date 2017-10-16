c9cafa15545285178d5bbb6762539ddaad081a16
TMJohnson1203 <tmjohnson1203@gmail.com>
2017-10-16 15:08:58 -0400

    initial commit

---
+++ Week2_Day4_Intro_OOP/Cat.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Week2_Day4_Intro_OOP
+{
+    class Cat
+    {
+        // STATES
+                // different states
+                        //fields
+        private string name;
+
+        // properties
+        private string Name // properties
+        {
+            get { return this.name;
+            }
+            set { this.name = value; }
+        }
+
+        // BEHAVIOR
+
+        // CONSTRUCTORS
+        public Cat()
+        {
+            // default constructor because it takes NO 
+            // parameters in the parentesis
+
+        }
+
+    }
+}
